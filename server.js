2d0054b70488139ae1521285e59ccf362faa1a6e
sorianopaul734-alt <sorianopaul734@gmail.com>
2026-06-22 06:23:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -342,11 +342,11 @@ app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
 
-const express = require('express');
+
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+
 app.use(cors());
 app.use(express.json());
 
